73bd9f53cf36d96f81d01b386a3bf72b13cf95b3
dzakiyaf <dzakiyafitriani@gmail.com>
2026-06-05 19:50:19 +0700

    membuat halaman food

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RestaurantController;
+use App\Http\Controllers\FoodController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/restaurants', [RestaurantController::class, 'index']);
+
+Route::get('/foods', [FoodController::class, 'index']);
+?>php
